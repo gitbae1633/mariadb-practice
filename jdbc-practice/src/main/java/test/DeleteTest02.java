b696a72167ab12ea0b3afc430148b9d18703dcb7
gitbae1633 <baeshop00@naver.com>
2023-01-17 13:29:56 +0900

    good

--- jdbc-practice/src/main/java/test/DeleteTest02.java
+++ jdbc-practice/src/main/java/test/DeleteTest02.java
@@ -22,7 +22,7 @@ public class DeleteTest02 {
 			Class.forName("org.mariadb.jdbc.Driver");
 			
 			//2. 연결하기
-			String url = "jdbc:mariadb://192.168.10.125:3307/webdb?charset=utf8";
+			String url = "jdbc:mariadb://192.168.10.105:3307/webdb?charset=utf8";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 			
 			//3. Statement 생성
